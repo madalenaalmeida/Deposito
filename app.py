78cf51544a45f65121528640fdebf7d9b77e3427
madalenaalmeida <madalenabarr@gmail.com>
2025-11-05 13:22:36 +0000

    Add Procfile and requirements for Koyeb

--- app.py
+++ app.py
@@ -238,3 +238,6 @@ if __name__ == "__main__":
             db.session.add(SiteConfig(orders_start=None, orders_end=None))
             db.session.commit()
     app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
